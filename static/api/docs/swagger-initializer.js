49447246103146ffe10a3a55efaa596579abd7bb
sunhokim <sunho.kim@bodycodi.com>
2023-01-30 07:55:25 +0900

    프로젝트 완성본

---
+++ static/api/docs/swagger-initializer.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    url: "openapi-3.0.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+};
